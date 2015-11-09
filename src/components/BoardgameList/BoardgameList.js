612d72fefbfe5e64d5f0ce5066cb3e5acb3e4d1c
Damian Prus <prus.damian@gmail.com>
2015-11-09 21:59:29 +0100

    Update BoardgameList component

--- src/components/BoardgameList/BoardgameList.js
+++ src/components/BoardgameList/BoardgameList.js
@@ -4,25 +4,26 @@ import { BoardgameListItem } from 'components';
 
 export default class BoardgameList extends Component {
   static propTypes = {
-    boardgames: PropTypes.array
-  }
-
-  handleCardClick(ev, gameId) {
-    console.log(ev.currentTarget, gameId);
+    boardgames: PropTypes.array,
+    handleCardClick: PropTypes.func
   }
 
   render() {
-    const { boardgames } = this.props;
+    const { boardgames, handleCardClick } = this.props;
     const styles = require('./BoardgameList.scss');
     return (
       <div>
         <ul className={styles.boardgameList}>
           {/* TODO: Make ReactCSSTransitionGroup work somehow */}
           <ReactCSSTransitionGroup transitionName="example" transitionEnterTimeout={300} transitionLeaveTimeout={300}>
-          {boardgames.map((game) => {
+          {boardgames.map((game, index) => {
             return (
-              <li className={styles.boardgameListItem} key={game.id} onClick={(ev) => this.handleCardClick(ev, game.id)}>
-                <BoardgameListItem name={game.name} year={game.year} />
+              <li className={styles.boardgameListItem} key={game.id} onClick={() => handleCardClick(index)}>
+                <BoardgameListItem
+                  thumbnail={game.thumbnail}
+                  name={game.name}
+                  year={game.year}
+                  score={game.score} />
               </li>
             );
           })}
