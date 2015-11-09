0b38c06700b1f411f1202e81b266fa6426e10922
Damian Prus <prus.damian@gmail.com>
2015-11-09 22:00:00 +0100

    Add staging actions

--- src/redux/modules/boardgames.js
+++ src/redux/modules/boardgames.js
@@ -1,10 +1,18 @@
 const QUERY = 'pipsy/boardgames/QUERY';
 const QUERY_SUCCESS = 'pipsy/boardgames/QUERY_SUCCESS';
 const QUERY_FAIL = 'pipsy/boardgames/QUERY_FAIL';
+const ADD_TO_STAGING = 'pipsy/boardgames/ADD_TO_STAGING';
+const REMOVE_FROM_STAGING = 'pipsy/boardgames/REMOVE_FROM_STAGING';
 
 const initialState = {
   isFetching: false,
   foundBoardgames: [],
+  currentInput: '',
+  responseReceived: false,
+  stagedBoardgames: [],
+  selectedPub: '',
+  availablePubs: [],
+  isUpdating: false
 };
 
 export default function reducer(state = initialState, action = {}) {
@@ -13,28 +21,82 @@ export default function reducer(state = initialState, action = {}) {
       return {
         ...state,
         isFetching: true,
-        foundBoardgames: []
+        currentInput: action.payload.query,
+        foundBoardgames: [],
+        responseReceived: false
       };
     case QUERY_SUCCESS:
       return {
         ...state,
         isFetching: false,
-        foundBoardgames: action.result
+        foundBoardgames: action.result,
+        responseReceived: true
       };
     case QUERY_FAIL:
       return {
         ...state,
         isFetching: false,
-        foundBoardgames: []
+        foundBoardgames: [],
+        responseReceived: true
+      };
+    case ADD_TO_STAGING:
+      if (state.stagedBoardgames.indexOf(state.foundBoardgames[action.payload.index]) !== -1) {
+        return state;
+      }
+      return {
+        ...state,
+        stagedBoardgames: [
+          ...state.stagedBoardgames,
+          state.foundBoardgames[action.payload.index]
+        ]
+      };
+    case REMOVE_FROM_STAGING:
+      return {
+        ...state,
+        stagedBoardgames: [
+          ...state.stagedBoardgames.slice(0, action.payload.index),
+          ...state.stagedBoardgames.slice(action.payload.index + 1)
+        ]
       };
     default:
       return state;
   }
 }
 
-export function load(query) {
+export function loadFromDB(query) {
   return {
     types: [QUERY, QUERY_SUCCESS, QUERY_FAIL],
-    promise: (client) => client.get('/boardgames/load?q=' + query )
+    payload: {
+      query
+    },
+    promise: (client) => client.get('/boardgames/loadFromDB?q=' + query)
+  };
+}
+
+export function loadFromBGG(query) {
+  return {
+    types: [QUERY, QUERY_SUCCESS, QUERY_FAIL],
+    payload: {
+      query
+    },
+    promise: (client) => client.get('/boardgames/loadFromBGG?q=' + query)
+  };
+}
+
+export function addToStaging(index) {
+  return {
+    type: ADD_TO_STAGING,
+    payload: {
+      index
+    }
+  };
+}
+
+export function removeFromStaging(index) {
+  return {
+    type: REMOVE_FROM_STAGING,
+    payload: {
+      index
+    }
   };
 }
