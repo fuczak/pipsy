8fdf3d709db89fc26d142d354b25f0298b79e05f
Damian Prus <prus.damian@gmail.com>
2015-11-14 14:54:08 +0100

    Remove unused links in navbar

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -76,13 +76,6 @@ export default class App extends Component {
 
             <ul className="nav navbar-nav">
               <li><NavbarLink to="/boardgames">Add Boardgames</NavbarLink></li>
-              {user && <li><NavbarLink to="/chat">Chat</NavbarLink></li>}
-
-              <li><NavbarLink to="/widgets">Widgets</NavbarLink></li>
-              <li><NavbarLink to="/survey">Survey</NavbarLink></li>
-              <li><NavbarLink to="/about">About Us</NavbarLink></li>
-              {!user && <li><NavbarLink to="/login">Login</NavbarLink></li>}
-              {user && <li className="logout-link"><a href="/logout" onClick={::this.handleLogout}>Logout</a></li>}
             </ul>
             {user &&
             <p className={styles.loggedInMessage + ' navbar-text'}>Logged in as <strong>{user.name}</strong>.</p>}
