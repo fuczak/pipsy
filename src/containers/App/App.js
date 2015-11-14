d099a14ac8fd43ec6997e4349e28e49fde2a1413
Damian Prus <prus.damian@gmail.com>
2015-11-14 15:01:20 +0100

    Clean up App.js a bit

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -32,21 +32,6 @@ export default class App extends Component {
     store: PropTypes.object.isRequired
   };
 
-  componentWillReceiveProps(nextProps) {
-    if (!this.props.user && nextProps.user) {
-      // login
-      this.props.pushState(null, '/loginSuccess');
-    } else if (this.props.user && !nextProps.user) {
-      // logout
-      this.props.pushState(null, '/');
-    }
-  }
-
-  handleLogout(event) {
-    event.preventDefault();
-    this.props.logout();
-  }
-
   render() {
     const {user} = this.props;
     const styles = require('./App.scss');
