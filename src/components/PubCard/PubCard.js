62264eae8f48bc266c6ece100d353e65ed8c4c26
Damian Prus <prus.damian@gmail.com>
2015-11-24 22:43:37 +0100

    Add map to PubCard component

--- src/components/PubCard/PubCard.js
+++ src/components/PubCard/PubCard.js
@@ -1,5 +1,6 @@
 import React, { Component, PropTypes } from 'react';
 import { Link } from 'react-router';
+import { PubMap } from 'components';
 
 export default class PubCard extends Component {
   static propTypes = {
@@ -11,10 +12,11 @@ export default class PubCard extends Component {
     const {pub, onDeletePub} = this.props;
     return (
       <div className="thumbnail">
-        <img src="http://placehold.it/400/400" />
+        {/* <img src="http://placehold.it/400/400" />*/}
+        <PubMap lat={pub.address.lat} lng={pub.address.lng} />
         <div className="caption">
           <h2>{pub.name}</h2>
-          <p>{pub._id}</p>
+          <p>{pub.address.street} <strong>{pub.address.number}</strong></p>
           <p>
             <Link to={'/pubs/' + pub._id} className="btn btn-primary">Edit</Link>
             <button className="btn btn-danger" onClick={onDeletePub}>Delete</button>
