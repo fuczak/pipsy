f9bc821e9a3c6ebfc48a58c3cfd3028bc368b58e
Damian Prus <prus.damian@gmail.com>
2015-11-27 22:53:30 +0100

    Extend functionality of PubSearch component

--- src/components/PubSearch/PubSearch.js
+++ src/components/PubSearch/PubSearch.js
@@ -3,13 +3,38 @@ import { debounce } from 'lodash';
 
 export default class PubSearch extends Component {
   static propTypes = {
-    onInputChange: PropTypes.func.isRequired
+    onInputChange: PropTypes.func.isRequired,
+    onListItemClick: PropTypes.func.isRequired,
+    onClear: PropTypes.func.isRequired,
+    foundPlaces: PropTypes.array
+  }
+
+  clearInput = () => {
+    this.searchbox.value = '';
+    this.props.onClear();
   }
 
   render() {
+    const styles = require('./PubSearch.scss');
     return (
-      <div>
-        <input className="form-control" type="text" onChange={debounce(this.props.onInputChange, 400)} />
+      <div className={styles.wrapper}>
+        <i className={styles.iconleft + ' fa fa-search'}></i>
+        <input className={styles.hasIcon + ' form-control'}
+          type="text"
+          ref={(ref) => this.searchbox = ref}
+          onChange={debounce(this.props.onInputChange, 400)} />
+        <ul className={styles.publist + ' list-group'}>
+          {this.props.foundPlaces.map((place) => {
+            return (
+              <li className={styles.item + ' list-group-item'}
+                key={place.id}
+                onClick={this.props.onListItemClick.bind(this, place.place_id, place.description)}>
+                {place.description}
+              </li>
+            );
+          })}
+        </ul>
+        <i className={styles.iconright + ' fa fa-times'} onClick={this.clearInput}></i>
       </div>
     );
   }
